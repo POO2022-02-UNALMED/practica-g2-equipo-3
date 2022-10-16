5e43774381347ee70ec604189bdc637b9f4f982d
JuanGomezR <111092528+JuanGomezR@users.noreply.github.com>
2022-10-16 17:14:56 -0500

    Update Main.java

--- src/uiMain/Main.java
+++ src/uiMain/Main.java
@@ -18,6 +18,8 @@ public class Main {
         Vuelo mc = new Vuelo(1, bus_unal, medellin, cucuta, "12:30", "13:20", "20/10/2022", 200000., "3C");
         Vuelo bb = new Vuelo(2, circular_coonatra, boyaca, bogota, "05:00", "06:00", "20/10/2022", 70000., "4A");
         Usuario Pepito = new Usuario(123, "1235", 100000);
+        Pasajero Alejandro = new Pasajero("Alejandro", 777);
+
         System.out.println("\n================================");
         System.out.println("AERO UNAL");
         System.out.println("================================");
